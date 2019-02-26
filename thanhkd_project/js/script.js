0a2f985f74240084a8dc26b157c29933fb324e75
thanhkhuatdac <thanhkhuatdac@gmail.com>
2019-02-27 02:02:19 +0700

    thanh_project part 3

--- thanhkd_project/js/script.js
+++ thanhkd_project/js/script.js
@@ -1,6 +1,6 @@
     // -------   Mail Send ajax
 
-     $(document).ready(function() {
+    $(document).ready(function() {
         var form = $('#myForm'); // contact form
         var submit = $('.submit-btn'); // submit button
         var alert = $('.alert-msg'); // alert div for show alert message
@@ -28,4 +28,9 @@
                 }
             });
         });
+
+        $('.gradient_btn').click(function() {
+            $('#form-modal').modal('show');
+        });
     });
+
